5807c255111c352c35a1539743d3b121c53330d4
Prajwal Gowda S <65961288+Prajwal518@users.noreply.github.com>
2021-02-15 20:12:20 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Heading from "./Heading";
+import Unorderedlist from "./Unorderedlist";
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <Unorderedlist></Unorderedlist>
+  </div>,
+  document.getElementById("root")
+);
